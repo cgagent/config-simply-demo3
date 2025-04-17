3bd42a7db8a15055c30c062f04fa033a2c8a4e15
Guy Levi <guyl@jfrog.com>
2025-04-17 20:40:21 +0300

    refactor: Remove unused export for CodeSnippets hook in CI setup
    - Deleted the export for the `useCodeSnippets` hook from the CI setup index file to streamline the codebase by removing unnecessary re-exports.

--- src/hooks/ci-setup/index.ts
+++ src/hooks/ci-setup/index.ts
@@ -3,4 +3,3 @@ export * from './useCISetupChat';
 export * from './useMessageManagement';
 export * from './usePackageSelection';
 export * from './useCISelection';
-export * from './useCodeSnippets';
